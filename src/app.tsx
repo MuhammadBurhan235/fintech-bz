c53397a8aae51eef3794d905029e4ec11bc26c09
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2025-09-08 11:27:57 +0700

    consume api

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import "./app.css";
 
 export function App() {
   const [users, setUsers] = useState<any[]>([]);
+  const [userId, setUserId] = useState("");
+  const [periduser, setPeriduser] = useState<any | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,6 +20,29 @@ export function App() {
       });
   }, []);
 
+  const handleSearch = async (e: Event) => {
+    e.preventDefault();
+    if (!userId) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch(`http://localhost:8080/api/users/${userId}`);
+
+      if (!res.ok) {
+        setPeriduser(null);
+        return;
+      }
+
+      const data = await res.json();
+      setPeriduser(data);
+    } catch (err) {
+      console.error("Error fetching user:", err);
+      setPeriduser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h1>Users List</h1>
@@ -32,6 +57,33 @@ export function App() {
               {user.full_name} — <b>{user.role}</b>
             </li>
           ))}
+          <div>
+            <h1>Search User by ID</h1>
+
+            {/* Form Input */}
+            <form onSubmit={handleSearch}>
+              <input
+                type="text"
+                placeholder="Enter User ID (UUID)"
+                value={userId}
+                onInput={(e: any) => setUserId(e.target.value)}
+              />
+              <button type="submit">Search</button>
+            </form>
+
+            {/* Hasil */}
+            <div style={{ marginTop: "1rem" }}>
+              {loading ? (
+                <p>Loading...</p>
+              ) : periduser ? (
+                <p>
+                  {periduser.full_name} — <b>{periduser.role}</b>
+                </p>
+              ) : (
+                <p>No user found</p>
+              )}
+            </div>
+          </div>
         </ul>
       )}
     </>
